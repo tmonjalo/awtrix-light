e450a8403a3c3c1a87573385597ff73c51217405
Thomas Monjalon <thomas@monjalon.net>
2023-12-29 22:51:23 +0100

    ring when reaching alarm time
    The time is checked at each tick.
    When it is time, the alarm will start ringing at the next ticks
    until minute changes or the select button is pressed.

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,5 +1,9 @@
 #include "timer.h"
 
+#include "Globals.h"
+#include "PeripheryManager.h"
+#include "DisplayManager.h"
+
 time_t now;
 struct tm *datetime;
 
@@ -15,6 +19,33 @@ struct tm *timer_localtime()
 
 void timer_tick()
 {
+    static bool ringing = false;
+    static time_t last_ring = 0;
+
     now = time(NULL);
     datetime = localtime(&now);
+
+    /* power on screen in advance of alarm */
+    if (datetime->tm_hour == (ALARM_HOUR - 1) && datetime->tm_min == ALARM_MINUTE)
+        DisplayManager.setPower(true);
+
+    if (ringing) {
+        if (datetime->tm_hour != ALARM_HOUR || datetime->tm_min != ALARM_MINUTE ||
+                PeripheryManager.buttonS->wasPressed()) {
+            /* end of alarm */
+            ringing = false;
+            PeripheryManager.stopSound();
+        } else if (!PeripheryManager.isPlaying()) {
+            /* start alarm sound */
+            PeripheryManager.playRTTTLString(MELODIES[ALARM_SOUND]);
+        }
+    } else {
+        if (datetime->tm_hour == ALARM_HOUR && datetime->tm_min == ALARM_MINUTE &&
+                now > (last_ring + 60) && ALARM_SOUND > 0) {
+            /* alarm */
+            ringing = true;
+            last_ring = now;
+            DisplayManager.setPower(true);
+        }
+    }
 }
